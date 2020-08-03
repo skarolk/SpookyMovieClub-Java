0027a7ccb2dab1e06bda2fdec5ef1c10f6d3e3b1
skarolk <skarolkiewicz@gmail.com>
2020-08-03 07:57:09 -0500

    Adding authorization

--- spooky_movie_club/src/main/java/com/spookymovieclub/app/ws/security/WebSecurity.java
+++ spooky_movie_club/src/main/java/com/spookymovieclub/app/ws/security/WebSecurity.java
@@ -16,22 +16,34 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	public WebSecurity(UserService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+
 		this.userDetailsService = userDetailsService;
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
-				.permitAll().anyRequest().authenticated().and()
-				.addFilter(new AuthenticationFilter(authenticationManager()));
+				.permitAll().anyRequest().authenticated().and().addFilter(getAuthenticationFilter())
+				.addFilter(new AuthorizationFilter(authenticationManager()));
 
 	}
 
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+
+	}
+
+	public AuthenticationFilter getAuthenticationFilter() throws Exception {
+
+		final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+		filter.setFilterProcessesUrl("/users/login");
+		return filter;
+
 	}
 
 }
