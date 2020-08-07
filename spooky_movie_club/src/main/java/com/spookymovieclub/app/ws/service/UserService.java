4a330c6bdd38580af6a6bec61b1a673ffb59a601
skarolk <skarolkiewicz@gmail.com>
2020-08-06 22:41:59 -0500

    added get users with pagination

--- spooky_movie_club/src/main/java/com/spookymovieclub/app/ws/service/UserService.java
+++ spooky_movie_club/src/main/java/com/spookymovieclub/app/ws/service/UserService.java
@@ -1,5 +1,7 @@
 package com.spookymovieclub.app.ws.service;
 
+import java.util.List;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import com.spookymovieclub.app.ws.shared.dto.UserDto;
@@ -15,5 +17,7 @@ public interface UserService extends UserDetailsService {
 	UserDto updateUser(String userId, UserDto user);
 
 	void deleteUser(String userId);
+
+	List<UserDto> getUsers(int page, int limit);
 
 }
--- spooky_movie_club/src/main/java/com/spookymovieclub/app/ws/ui/controller/UserController.java
+++ spooky_movie_club/src/main/java/com/spookymovieclub/app/ws/ui/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.spookymovieclub.app.ws.ui.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spookymovieclub.app.ws.exceptions.UserServiceException;
@@ -97,4 +101,28 @@ public class UserController {
 		return returnValue;
 
 	}
+
+	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	public List<UserRest> getUsers(@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "limit", defaultValue = "25") int limit) {
+
+		if (page > 0)
+			page -= 1;
+
+		List<UserRest> returnValue = new ArrayList<>();
+
+		List<UserDto> users = userService.getUsers(page, limit);
+
+		for (UserDto userDto : users) {
+
+			UserRest userModel = new UserRest();
+			BeanUtils.copyProperties(userDto, userModel);
+			returnValue.add(userModel);
+
+		}
+
+		return returnValue;
+
+	}
+
 }
